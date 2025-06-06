f07271a430bd7c17c2d481f639f712b392649ed0
chinmay768 <chinmaysahani07@gmail.com>
2025-06-06 23:44:26 +0530

    Check Array Sorted Rotated

---
+++ striver/src/CheckIfArrayIsSorted.java
@@ -0,0 +1,25 @@
+public class CheckIfArrayIsSorted {
+
+    public static boolean checkSorted(int[] nums){
+        int len = nums.length;
+        if(len == 1) return true;
+
+        int count = 1;
+        for(int i = 2; i < 2 * len; i++){
+            if(nums[(i - 1) % len] <= nums[i % len]){
+                count++;
+            }else
+                count = 1;
+
+            if(count == len) return true;
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {3,4,5,1,2};
+
+        System.out.println(checkSorted(arr));
+    }
+}
